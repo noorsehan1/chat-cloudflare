56367061b6a6335c93acef2a02d28b2e67812598
noorsehan1 <42403280+noorsehan1@users.noreply.github.com>
2025-12-08 22:51:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { LowCardGameManager } from "./lowcard.js";
 import { VipBadgeManager } from "./vipbadge.js";
 
 const roomList = [
-  "LowCard", "General", "HINDI", "Indonesia", "Chill Zone", "Catch Up", "Casual Vibes", "Lounge Talk",
+  "LowCard", "General", "HINDI", "Indonesia","MEPHISTOPHELES","Chill Zone", "Catch Up", "Casual Vibes", "Lounge Talk",
   "Easy Talk", "Friendly Corner", "The Hangout", "Relax & Chat", "Just Chillin", "The Chatter Room"
 ];
 
@@ -1384,3 +1384,4 @@ export default {
     }
   }
 };
+
