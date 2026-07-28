3f23aa269a899e64ccaf8aff79f97a9b29dd7684
noorsehan1 <42403280+noorsehan1@users.noreply.github.com>
2026-07-28 19:25:31 +0800

    Update quiz session time range

--- game-server.js
+++ game-server.js
@@ -63,7 +63,7 @@ const QUIZ_SCHEDULE = {
   SESSIONS: [
     { start: 1, end: 2 },
     { start: 11, end: 12 },
-    { start: 22, end: 23 }
+    { start: 19, end: 20 }
   ],
   TIMEZONE_OFFSET: 8,
 };
