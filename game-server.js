b1daf29ab0f4d2d45afe2347d8cc11af49dd75d2
noorsehan1 <42403280+noorsehan1@users.noreply.github.com>
2026-07-29 17:45:26 +0800

    Update game-server.js

--- game-server.js
+++ game-server.js
@@ -72,8 +72,8 @@ const CONSTANTS = {
 
 const QUIZ_SCHEDULE = {
   SESSIONS: [
-    { start: 1, end: 6 },
-    { start: 11, end: 12 },
+    { start: 1, end: 2 },
+    { start: 11, end: 18 },
     { start: 23, end: 24 }
   ],
   TIMEZONE_OFFSET: 8,
